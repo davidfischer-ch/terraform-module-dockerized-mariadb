e2c5880bfa930d86f6dc79584cd43e78b69f521f
David Fischer <david@fisch3r.net>
2024-11-17 23:36:19 +0100

    Drop outputs

--- outputs.tf
+++ outputs.tf
@@ -6,19 +6,6 @@ output "port" {
   value = var.port
 }
 
-output "name" {
-  value = var.name
-}
-
-output "user" {
-  value = var.user
-}
-
-output "password" {
-  value     = var.password
-  sensitive = true
-}
-
 output "root_password" {
   value     = random_password.root_password.result
   sensitive = true
